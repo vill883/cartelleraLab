fd37ffc547aa9cafa5e427db0e6425adb4666ab7
vill883 <raulvillafranca86@gmail.com>
2016-10-13 00:43:47 +0200

    Raul

--- src/main/java/com/tecnocampus/domain/UsuariBuilder.java
+++ src/main/java/com/tecnocampus/domain/UsuariBuilder.java
@@ -6,62 +6,54 @@ package com.tecnocampus.domain;
 public class UsuariBuilder {
 
     private String DNI;
-    private String titol;
+    private String nom;
     private String cognom;
     private int telefon;
     private String idUsuari;
     private String password;
 
-    public String getDNI() {
-        return DNI;
-    }
 
 
-    public void setDNI(String DNI) {
+
+    public UsuariBuilder setDNI(String DNI) {
         this.DNI = DNI;
+        return this;
     }
 
-    public String getTitol() {
-        return titol;
+
+
+    public UsuariBuilder setNom(String nom) {
+        this.nom = nom;
+        return this;
     }
 
-    public void setTitol(String titol) {
-        this.titol = titol;
-    }
-
-    public String getCognom() {
-        return cognom;
-    }
-
-    public void setCognom(String cognom) {
+    public UsuariBuilder setCognom(String cognom) {
         this.cognom = cognom;
+        return this;
     }
 
-    public int getTelefon() {
-        return telefon;
-    }
 
-    public void setTelefon(int telefon) {
+    public UsuariBuilder setTelefon(int telefon) {
         this.telefon = telefon;
+        return this;
     }
 
-    public String getIdUsuari() {
-        return idUsuari;
-    }
 
-    public void setIdUsuari(String idUsuari) {
+
+    public UsuariBuilder setIdUsuari(String idUsuari) {
         this.idUsuari = idUsuari;
+        return this;
     }
 
-    public String getPassword() {
-        return password;
-    }
 
-    public void setPassword(String password) {
+
+
+    public UsuariBuilder setPassword(String password) {
         this.password = password;
+        return this;
     }
 
     public Usuari createUsuari() {
-        return new Usuari(DNI, titol, cognom, telefon, idUsuari, password );
+        return new Usuari(DNI, nom, cognom, telefon, idUsuari, password );
     }
 }
